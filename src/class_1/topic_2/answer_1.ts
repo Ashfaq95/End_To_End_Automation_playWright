8637758036a00034b861c01b18d1f3ab31b2fdb3
Ashfaqcefalo <asfaqur.rahman@cefalo.com>
2024-05-20 17:39:33 +0600

    Updated T2_Q1 as per shantanu bhai's suggestion

--- src/class_1/topic_2/answer_1.ts
+++ src/class_1/topic_2/answer_1.ts
@@ -1,10 +1,10 @@
-const title: string = "Muhammad";
-const firstName: string = "   Ashfaqur     ";
+// const title: string = "Muhammad";
+const firstName: string = "  Mr. Ashfaqur     ";
 const middleName: string | null = "  Rahman   ";
 const lastName: string = "   Amul khondokar  ";
 
 // to Remove extra spaces used .trim() 
-const trimmedTitle: string = title.trim();
+// const trimmedTitle: string = title.trim();
 const trimmedFirstName: string = firstName.trim();
 const trimmedMiddleName: string | null = middleName && middleName.trim() || null;
 
@@ -13,14 +13,14 @@ const trimmedLastName: string = lastName.trim();
 // join the whole name 
 //Here .filter(Boolean) filters the string array if there's any unwanted values(ex: null, undefined etc)
 //.join(' ') takes the filtered array and  joins all the elements in a single array
-const joinFullName: string = [trimmedTitle, trimmedFirstName, trimmedMiddleName, trimmedLastName].filter(Boolean).join(' ');
+const fullName: string = [firstName.trim(), middleName && middleName.trim(), lastName.trim()].filter(Boolean).join(' ');
 
-console.log(joinFullName);
+console.log(fullName);
 
 // Validation
 const maxLength: number = 35;
 //to measure extra char above 35
-const extraChars: number = joinFullName.length - maxLength;
+const extraChars: number = fullName.length - maxLength;
 //defined exeedMessage
 //used Math.abs() to visualize the value of n. Math.abs will show the absolute value of extra numbers
 const exceedMsg: string = `Your input exceeds the character limit of ${maxLength} by ${Math.abs(extraChars)} characters.`;
